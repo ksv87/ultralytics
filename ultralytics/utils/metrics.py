6810996a97744fc5ff0e005c3d49002af16c5d79
Sergey <smska87@bk.ru>
2026-04-01 15:15:20 +0300

    Update metrics.py
    Signed-off-by: Sergey <smska87@bk.ru>

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -316,7 +316,7 @@ class ConfusionMatrix(DataExportMixin):
         matches (dict | None): Contains the indices of ground truths and predictions categorized into TP, FP and FN.
     """
 
-    def __init__(self, names: dict[int, str] = [], task: str = "detect", save_matches: bool = False):
+    def __init__(self, names: dict[int, str] = {}, task: str = "detect", save_matches: bool = False):
         """Initialize a ConfusionMatrix instance.
 
         Args:
